60aad266b2967f09b135ab2632b11fd56cea9319
Ily1606 <no1.ily1606@gmail.com>
2022-11-21 00:28:34 +0700

    build: cache control image

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
+import moment from "moment";
 import routers from "./router/router.js";
 import rateLimitRequest from "./util/rateLimitRequest.js";
 import response from "#response";
 dotenv.config();
 const app = express();
 const PORT_SERVER = 3000;
+const MAX_AGE_CACHE = 60*60*24*30; // 1 month = 2592000 s
 
 /**
  * //TODO Using libraries for expressjs
@@ -17,7 +19,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(rateLimitRequest);
 app.use(cors()); // Enable cors for all origins
-app.use(express.static("storage"));
+app.use(express.static("storage",{
+	setHeaders: function(res) {
+		res.set("Pragma", "public");
+		res.set("Cache-Control", `max-age=${MAX_AGE_CACHE}`);
+		res.set("Expires", moment(new Date().getTime() + MAX_AGE_CACHE *1000).format("ddd, DD MMM YYYY HH:mm:ss")+" GMT");
+	}
+}));
 
 /**
  // TODO Dynamic router, middleware
